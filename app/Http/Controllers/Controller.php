0955abbe8e84e641a39989dbd1b3fd3f84a645a2
NINEL972 <114186472+NINEL972@users.noreply.github.com>
2022-10-10 15:26:31 +0000

    controller effectuer

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -11,3 +11,15 @@ class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 }
+
+class WelcomeController extends Controller
+{
+public function index()
+{
+return view('welcome');
+}
+}
+//le premier paramètre correspond à l'URL
+//le second paramètre spécifie le contrôleur et la méthode à appeler.
+//name permet de donner un alias à la route (utile pour les liens vers la page)
+Route::get('/','WelcomeController@index')->name('accueil');
