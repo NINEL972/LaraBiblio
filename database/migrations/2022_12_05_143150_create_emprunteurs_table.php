2dc9348272fbf7be276be504fd2256e08a8a3c7d
NINEL972 <114186472+NINEL972@users.noreply.github.com>
2022-12-12 15:22:27 +0000

    migrate

--- database/migrations/2022_12_05_143150_create_emprunteurs_table.php
+++ database/migrations/2022_12_05_143150_create_emprunteurs_table.php
@@ -14,8 +14,19 @@ return new class extends Migration
     public function up()
     {
         Schema::create('emprunteurs', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+            //l'attribut "numeroCarte" est un entier
+            $table->integer('numeroCarte');
+            //les attributs "nom" et « prénom » sont des chaînes qui peuvent être NULL
+            $table->string('nom', 30)->nullable();
+            $table->string('prenom', 30)->nullable();
+            //l'attribut "adresse" est une chaîne qui peut être NULL
+            $table->text('adresse')->nullable();
+            //l'attribut "telephone" est un entier qui peut être NULL
+            $table->integer('telephone')->nullable();
+            //la clé primaire est "numeroCarte"
+            $table->primary('numeroCarte');
+            //le jeu de caractères à utiliser
+            $table->charset = 'utf8mb4';
         });
     }
 
